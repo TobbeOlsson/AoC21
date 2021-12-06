989d4841f2c2586590cb21faceb91bafdae30d7b
Tobias Olsson <tobias94olsson@gmail.com>
2021-12-06 18:05:31 +0100

    Day 4 finished

--- day4.cpp
+++ day4.cpp
@@ -6,7 +6,7 @@
 #include <sstream>
 
 void mark_board(int num, int board[][5]){
-    for (size_t i = 0; i < 15; i++){
+    for (size_t i = 0; i < 500; i++){
         for (size_t j = 0; j < 5; j++){
             if (board[i][j] == num){
                 board[i][j] += 1000;
@@ -16,10 +16,11 @@ void mark_board(int num, int board[][5]){
     return;
 }
 
+/* Returns first winning board or -1 */
 int find_winner(int board[][5]){
     int board_index = 0;
     bool winner_found = false;
-    while (!winner_found && board_index < 495){
+    while (!winner_found && board_index < 500){
         for (size_t i = 0; i < 4; i++){
             if (board[board_index + i][0] + board[board_index + i][1] + 
             board[board_index + i][2] + board[board_index + i][3] + board[board_index + i][4] > 5000) {
@@ -38,6 +39,30 @@ int find_winner(int board[][5]){
     return -1;
 }
 
+/* Returns number of winning boards and updates number_of_wins[] for all winning boards */ 
+int num_of_winners(int board[][5], int number_of_wins[]){
+    int board_index = 0;
+    int winners_found = 0;
+    while (board_index < 500){
+        for (size_t i = 0; i <= 4; i++){
+            if (board[board_index + i][0] + board[board_index + i][1] + 
+            board[board_index + i][2] + board[board_index + i][3] + board[board_index + i][4] > 5000) {
+                winners_found++;
+                number_of_wins[board_index / 5]++;
+                break;
+            }
+            if (board[board_index + 0][i] + board[board_index + 1][i] + 
+            board[board_index + 2][i] + board[board_index + 3][i] + board[board_index + 4][i] > 5000){
+                winners_found++;
+                number_of_wins[board_index / 5]++;
+                break;
+            } 
+        }
+        board_index += 5;
+    }
+    return winners_found;
+}
+
 int calculate_score(int winner, int last_drawn, int board[][5]){
    int board_index = winner * 5 - 5;
    int sum = 0;
@@ -51,50 +76,10 @@ int calculate_score(int winner, int last_drawn, int board[][5]){
     return sum * last_drawn;
 }
 
-int find_last_winner(int board[][5], int (&won)[3]){
-    int board_index = 0;
-    int last_won = -1;
-    int num_winners = 0;
-    bool winner_found = false;
-    while (!winner_found && board_index < 15){
-        for (size_t i = 0; i < 4; i++){
-            if (board[board_index + i][0] + board[board_index + i][1] + 
-            board[board_index + i][2] + board[board_index + i][3] + board[board_index + i][4] > 5000) {
-                std::cout << "WINNER WINNER: " << board_index /5 << "\n";
-                std::cout << "SCORE SCORE: " << calculate_score(2, 24, board) << "\n";
-                won[board_index / 5] = 1;
-            }
-            if (board[board_index + 0][i] + board[board_index + 1][i] + 
-            board[board_index + 2][i] + board[board_index + 3][i] + board[board_index + 4][i] > 5000){
-                std::cout << "WINNER WINNER2: " << board_index /5 << "\n";
-                std::cout << "SCORE SCORE2: " << calculate_score(2, 24, board) << "\n";
-                won[board_index / 5] = 1;
-            } 
-        }
-        board_index += 5;
-    }
-    for(int i = 0; i < 3; i++){
-        if(won[i] == 1){
-            num_winners++;
-            last_won = i;
-        } else{
-            //do nothing
-        }
-    }
-    std::cout << "Num of winners: " << num_winners;
-    if (num_winners > 2){
-        return last_won;
-    }
-    return last_won;
-}
-
-
-
 int main(){
     std::vector<int> drawn_nums;
-    std::ifstream in("inputs/input_ex");
-    std::vector<int> winners;
-    int board[15][5];
+    std::ifstream in("inputs/input_day4");
+    int board[500][5];
     std::string line;
     int i = 0, winner = -1, score = 0;
     while(std::getline(in, line)){
@@ -104,7 +89,7 @@ int main(){
             while (std::getline(ss, s, ',')){
                 drawn_nums.push_back(std::stoi(s));
             }
-        } else {  //read bingo boards
+        } else {  //read boards
             std::string s;
             while (in >> board[i][0] >> board[i][1] >> board[i][2] >> board[i][3] >> board[i][4]){
                 i++;
@@ -112,31 +97,24 @@ int main(){
         }
     }
 
-    int won[3] = { };
+    int number_of_wins[100] = { };
+    int winning_boards = 0;
     for(int num : drawn_nums){
         mark_board(num, board);
-        std::cout << "Marked " << num << "\n"; 
-        if(winners.size() < 3){
-            winner = find_last_winner(board, won);
-            if (winner != -1){
-                std::cout << "Winner: " << winner << "\n";
-                //winners.push_back(winner);
-                score = calculate_score(winner, num, board);
-                std::cout << "Score: " << score << "\n";
+        winning_boards = num_of_winners(board, number_of_wins);
+        /* Find index of winning board in number_of_wins[] */
+        if (winning_boards >= 100){
+            int winner_index = 0;
+            for (size_t i = 0; i < 100; i++){
+                if (number_of_wins[i] <= number_of_wins[winner_index]) {
+                    winner_index = i;
+                }
             }
-            //if(winners.size() == 100){
-                //score = calculate_score(winner, num, board);
-                //break;
-            //}
-        }
-        // winner = find_last_winner(board, winners);
-        // if(winner != -1){
-        //     score = calculate_score(winner, num, board);
-        //     break;
-        // }
-        
+            std::cout << "Last winner: " << winner_index + 1 << "\n";
+            score = calculate_score(winner_index + 1, num, board);
+            break;
+        }     
     }
-    std::cout << "Winner is board: " << winner << "\n";
     std::cout << "Score: " << score << "\n";
     return 0;
 }
