9c0e7bc70f66046cc1ae2a5aad3b0cdde6dd6930
Tobias Olsson <tobias94olsson@gmail.com>
2021-12-06 01:09:04 +0100

    Day 5 finished

--- day5.cpp
+++ day5.cpp
@@ -9,7 +9,6 @@ int main(){
     std::ifstream in("inputs/input_day5");
     std::string line;
     int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-    std::cout << "Should be zero: " << sea_floor[4][6] << "\n";
     while(std::getline(in, line)){
         std::string line_after_arrow = line.substr(line.find('>') + 1, line.length() - 2);
         x1 = std::stoi(line.substr(0, line.find(',')));
@@ -17,46 +16,67 @@ int main(){
         x2 = std::stoi(line_after_arrow.substr(0, line_after_arrow.find(',')));
         y2 = std::stoi(line_after_arrow.substr(line_after_arrow.find(',') + 1, line_after_arrow.length() - 2));
 
-        //std::cout << x2 << " ";
-        //read one row, add 1 to all coordinates in matrix.
-
         if (x1 == x2){
             if(y1 > y2){
                 for(int i = 0; i <= (y1 - y2); i++){
-                    sea_floor[y1 + i][x1]++;
-                    //std::cout << "sea_floor " << x1 << " " << y2 + i  << " " << sea_floor[x1][y2 + i] << "\n";
+                    sea_floor[x1][y2 + i]++;
                 }
             } else if (y2 > y1) {
                 for(int i = 0; i <= (y2 - y1); i++){
-                    sea_floor[y1 + i][x1]++;
-                    //std::cout << "sea_floor " << x1 << " " << y1 + i << " " << sea_floor[x1][y1 + i] << "\n";
+                    sea_floor[x1][y1 + i]++;
                 }
             }
         } else if (y1 == y2){
             if(x1 > x2){
                 for(int i = 0; i <= (x1 - x2); i++){
-                    sea_floor[y1][x2 + i]++;
-                    //std::cout << "sea_floor " << y1 << " " << x2 + i << " " << sea_floor[x2 + i][y1] << "\n";
+                    sea_floor[x2 + i][y1]++;
                 }
             } else if (x2 > x1) {
                 for(int i = 0; i <= (x2 - x1); i++){
-                    sea_floor[y1][x1 + i]++;
-                    //std::cout << "sea_floor " << y1 << " " << x1 + i << " " << sea_floor[x1 + i][y1] << "\n";
+                    sea_floor[x1 + i][y1]++;
+                }
+            }
+        } else {
+            if(x1 == y1 && x2 == y2){
+                if(x1 > x2){
+                    for(int i = 0; i <= (x1 - x2); i++){
+                        sea_floor[x1 - i][y1 - i]++;
+                    }
+                } else {
+                    for(int i = 0; i <= (x2 - x1); i++){
+                        sea_floor[x1 + i][y1 + i]++;
+                    }
+                }
+            } else if(x1 > x2){
+                if(y1 > y2){
+                    for(int i = 0; i <= (x1 - x2); i++){
+                        sea_floor[x1 - i][y1 - i]++;
+                    }
+                } else {
+                    for(int i = 0; i <= (x1 - x2); i++){
+                        sea_floor[x1 - i][y1 + i]++;
+                    }
+                }
+            } else if(x2 > x1){
+                if(y2 > y1){
+                    for(int i = 0; i <= (x2 - x1); i++){
+                        sea_floor[x1 + i][y1 + i]++;
+                    }
+                } else {
+                    for(int i = 0; i <= (x2 - x1); i++){
+                        sea_floor[x1 + i][y1 - i]++;
+                    }
                 }
             }
         }
     }
-    std::cout << "Finished with reading inputs\n";
-    // go throw all matrix and check values > 1
+
     int score = 0;
     for (int i = 0; i < 1000; i++){
         for (int j = 0; j < 1000; j++){
             if (sea_floor[i][j] > 1) {
-                std::cout << sea_floor[i][j];
-                std::cout << "Score 2 location " << i << " " << j << "\n";
                 score++;
             }
-        std::cout << "\n";
         }
     }
     std::cout << "Score: " << score << "\n";
