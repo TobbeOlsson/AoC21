9f916fede92f17984a9fba98ba809ef6cbac2dec
Tobias Olsson <tobias94olsson@gmail.com>
2021-12-14 01:41:08 +0100

    Further progress on day 13

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CC = g++
 
 CFLAGS  = -g -fsanitize=address -Wall
 
-TARGET = day1 day2 day3 day4 day5 day6 day7 day8 day9 day10 day11
+TARGET = day1 day2 day3 day4 day5 day6 day7 day8 day9 day10 day11 day13
 
 all: $(TARGET)
 
--- day13.cpp
+++ day13.cpp
@@ -9,6 +9,7 @@
 
 void drawPaper(std::vector<std::pair<int, int>> dots){
     //should not be static size 15, should be biggest x, 11 biggest y?
+    std::cout << "drawing\n";
     bool matrix[11][15] = { false };
     for (std::pair<int, int> p : dots){
         matrix[p.first][p.second] = true;
@@ -26,32 +27,50 @@ void drawPaper(std::vector<std::pair<int, int>> dots){
     std::cout << "\n";
 }
 
-void foldPaper(std::vector<std::pair<int, int>>& dots, std::queue<std::pair<char, int>>& fold_queue){
-    int fold_line = fold_queue.front().second;
-    if(fold_queue.front().first == 'x'){
-        for(std::pair<int, int> p : dots){
-            if(p.first > fold_line){ //move dot left of fold line
-                p.first = (p.first / 2 )- 1;
+void foldPaper(std::vector<std::pair<int, int>>& dots, std::vector<std::pair<char, int>>& fold_queue){
+    int fold_line = fold_queue.back().second;
+    int newx = 0; int newy = 0;
+    if(fold_queue.back().first == 'x'){
+        for(int i = 0; i < dots.size(); i++){
+            if(dots[i].first > fold_line){ //move dot left of fold line
+                newx = (fold_line * 2) - dots[i].first;
+                dots.push_back(std::make_pair(newx, dots[i].second));
+                std::cout << "xxxx\n"; 
             }
         }
 
-    } else if (fold_queue.front().first == 'y'){
-        for(std::pair<int, int> p : dots){
-            if(p.second > fold_line){ //move dot left of fold line
-                p.second = (p.second / 2 )- 1;
+    } else if (fold_queue.back().first == 'y'){
+        std::cout << "yyyy\n";
+        for(int i = 0; i < dots.size(); i++){
+            if(dots[i].second > fold_line){ //move dots above of fold line
+                newy = (fold_line * 2) - dots[i].second;
+                dots.push_back(std::make_pair(dots[i].first, newy));
             }
         }
     }
-    fold_queue.pop();
+    std::cout << "after\n"; 
+    fold_queue.pop_back();
 }
 
 int countDots(std::vector<std::pair<int, int>> dots){
-    return 0;
+    int dot = 0;
+    bool matrix[11][15] = { false };
+    for (std::pair<int, int> p : dots){
+        matrix[p.first][p.second] = true;
+    }
+    for(int y = 0; y < 8; y++){
+        for(int x = 0; x < 11; x++){
+            if(matrix[x][y]){
+                dot++;
+            }
+        }
+    }
+    return dot;
 }
 
 int main(){
     std::vector<std::pair<int, int>> dots;
-    std::queue<std::pair<char, int>> fold_queue;
+    std::vector<std::pair<char, int>> fold_queue;
     std::ifstream in("inputs/input_ex13");
     std::string line;
 
@@ -66,9 +85,16 @@ int main(){
             std::string fold_direction, fold_index;
             fold_direction = line.substr(line.find('=') - 1, 1);
             fold_index = line.substr(line.find('=') + 1, line.length() - 2);
-            fold_queue.push(std::make_pair(fold_direction[0], std::stoi(fold_index)));
+            fold_queue.insert(fold_queue.begin(), std::make_pair(fold_direction[0], std::stoi(fold_index)));
         }
     }
+
+    //In most funcs I need to keep track of the current size of our paper
+    //therefore not working with bigger input atm. Example working
+
+    drawPaper(dots);
+    foldPaper(dots, fold_queue);
+    std::cout << "num of dots " << countDots(dots) << "\n";
     drawPaper(dots);
     foldPaper(dots, fold_queue);
     drawPaper(dots);
